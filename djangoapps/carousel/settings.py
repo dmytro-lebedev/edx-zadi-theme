9720d86e3b2dab281108b0e8408af6138aa28715
Dmitry Lebedev <dmytro.lebedev@gmail.com>
2014-06-25 17:34:58 +0300

    Fix settings.py

--- djangoapps/carousel/settings.py
+++ djangoapps/carousel/settings.py
@@ -1,4 +1,4 @@
 from django.conf import settings
 
 MEDIA_ROOT = '/edx/var/edxapp/staticfiles/themes/{}/uploads'.format(settings.THEME_NAME)
-MEDIA_URL = '/static/themes/{}/uploads/'.format(settings.THEME_NAME)
+MEDIA_URL = '/static/themes/{}/uploads'.format(settings.THEME_NAME)
