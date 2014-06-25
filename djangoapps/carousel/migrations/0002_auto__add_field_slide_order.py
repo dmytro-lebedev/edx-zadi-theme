62a59e864a68091f5672edb1b2025a840667c974
Dmitry Lebedev <dmytro.lebedev@gmail.com>
2014-06-25 10:24:51 -0500

    Add migration

---
+++ djangoapps/carousel/migrations/0002_auto__add_field_slide_order.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Slide.order'
+        db.add_column('carousel_slide', 'order',
+                      self.gf('django.db.models.fields.PositiveIntegerField')(default=0),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Slide.order'
+        db.delete_column('carousel_slide', 'order')
+
+
+    models = {
+        'carousel.slide': {
+            'Meta': {'ordering': "['order']", 'object_name': 'Slide'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'order': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['carousel']
