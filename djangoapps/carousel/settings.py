6e008a3cac4d4c7846327dbd9c5c157eae53d811
Dmitry Lebedev <dmytro.lebedev@gmail.com>
2014-06-25 16:41:33 +0300

    Update settings.py

--- djangoapps/carousel/settings.py
+++ djangoapps/carousel/settings.py
@@ -1,8 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from lms.urls import urlpatterns
+from lms import urls
 
 THEME_ROOT = settings.ENV_ROOT / 'themes' / settings.THEME_NAME
 settings.MEDIA_ROOT = THEME_ROOT / 'uploads'
 settings.MEDIA_URL = '/uploads/'
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urls.urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
