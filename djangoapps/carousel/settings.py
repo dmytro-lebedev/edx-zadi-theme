3c955367df070e1a37d20f6735cade4f293bd1ce
Dmitry Lebedev <dmytro.lebedev@gmail.com>
2014-06-25 16:09:00 +0300

    Update settings.py

--- djangoapps/carousel/settings.py
+++ djangoapps/carousel/settings.py
@@ -1,9 +1,5 @@
 from django.conf import settings
 
 THEME_ROOT = settings.ENV_ROOT / 'themes' / settings.THEME_NAME
-THEME_MEDIA_ROOT = THEME_ROOT / 'uploads'
-THEME_MEDIA_URL = 'uploads'
-
-settings.STATICFILES_DIRS.append(
-    (THEME_MEDIA_URL, THEME_MEDIA_ROOT)
-)
+THEME_MEDIA_ROOT = THEME_ROOT / 'static' / 'uploads'
+THEME_MEDIA_URL = 'themes/{}/uploads'.format(settings.THEME_NAME)
