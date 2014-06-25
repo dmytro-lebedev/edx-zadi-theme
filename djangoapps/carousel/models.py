ea54b58fd36932610b2d3ba78b839f2dffaac065
Dmitry Lebedev <dmytro.lebedev@gmail.com>
2014-06-25 11:21:27 +0300

    Update models.py

--- djangoapps/carousel/models.py
+++ djangoapps/carousel/models.py
@@ -1,10 +1,11 @@
 from django.db.models import Model
 from django.db.models.fields import CharField, URLField, BooleanField
 from django.db.models.fields.files import ImageField
+from django.core.files.storage import FileSystemStorage
 
 class Slide(Model):
     title = CharField(max_length=255)
     description = CharField(max_length=255)
     url = URLField(blank=True)
-    image = ImageField(upload_to='carousel/slides')
+    image = ImageField(storage=FileSystemStorage(), upload_to='carousel/slides')
     active = BooleanField()
