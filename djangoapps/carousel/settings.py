c68d4e655d2b50cb87667c444a636a7c9d8253c3
Dmitry Lebedev <dmytro.lebedev@gmail.com>
2014-06-25 15:46:54 +0300

    Fix carousel.html, update settings.py

--- djangoapps/carousel/settings.py
+++ djangoapps/carousel/settings.py
@@ -2,7 +2,7 @@ from django.conf import settings
 
 THEME_ROOT = settings.ENV_ROOT / 'themes' / settings.THEME_NAME
 THEME_MEDIA_ROOT = THEME_ROOT / 'uploads'
-THEME_MEDIA_URL = 'themes/{}/uploads'.format(settings.THEME_NAME)
+THEME_MEDIA_URL = 'uploads'
 
 settings.STATICFILES_DIRS.append(
     (THEME_MEDIA_URL, THEME_MEDIA_ROOT)
